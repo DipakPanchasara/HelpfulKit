9422f016969ba0c8699670dd1364803f1614e366
Dipak Panchasara <p>
2022-12-29 12:43:37 +0530

    Nullable Added

---
+++ HelpfulKit/Nullable.swift
@@ -0,0 +1,47 @@
+//
+//  Nullable.swift
+//  HelpfulKit
+//
+//  Created by Dipak Panchasara on 29/12/22.
+//
+
+import Foundation
+
+public enum Nullable<T: Codable>: Codable {
+    case null
+    case value(T)
+    
+    public init(_ optional: Optional<T>) {
+        switch optional {
+        case .some(let value):
+            self = .value(value)
+        case .none:
+            self = .null
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .null:
+            try container.encodeNil()
+        case .value(let value):
+            try container.encode(value)
+        }
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        do {
+            let value = try container.decode(T.self)
+            self = .value(value)
+        } catch {
+            self = .null
+        }
+    }
+    
+    public var value: T? {
+        guard case let .value(value) = self else { return nil }
+        return value
+    }
+}
